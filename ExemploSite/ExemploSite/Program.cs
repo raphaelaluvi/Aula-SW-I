246d3258e3078ef7a117a4d87320cf7b8fe44928
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-04-11 18:41:11 -0300

    Add files via upload

---
+++ ExemploSite/ExemploSite/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExemploSite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Rapha";
+            p.DataNasc = new DateTime(2007, 9, 24);
+            p.Altura = 1.60f;
+
+            p.ImprimirDados();
+
+            int idade = p.CalcularIdade();
+
+            Console.WriteLine("Idade: " + idade);
+        }
+    }
+}
