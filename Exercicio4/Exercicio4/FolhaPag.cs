3001fc70520ee022dc4542fb28573e90133302b3
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-04-12 12:14:50 -0300

    Add files via upload

---
+++ Exercicio4/Exercicio4/FolhaPag.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    public class FolhaPag
+    {
+        public string? Nome { get; set; }
+        public double Sal { get; set; }
+        public double Hed { get; set; }
+        public double Hen { get; set; }
+        public double Nd { get; set; }
+        public double Fal { get; set; }
+        public double De { get; set; }
+        public double Ref { get; set; }
+        public double Val { get; set; }
+        public double HoraExtra { get; set; }
+        public double SalF { get; set; }
+        public double SalB { get; set; }
+        public double INAMPS { get; set; }
+        public double Faltas { get; set; }
+        public double ImpR { get; set; }
+        public double DescFinal { get; set; }
+        public double SalLiq { get; set; }
+
+        public void Calc()
+        {
+            HoraExtra = Hed * Sal / 160 + Hen * 1.2 * Sal / 160;
+            SalF = Nd * 0.05 * 1.371;
+            SalB = Sal + HoraExtra + SalF;
+
+            INAMPS = 0.08 * Sal;
+            Faltas = Fal * Sal / 160;
+            ImpR = 0.08 * SalB;
+
+            DescFinal = INAMPS + Faltas + ImpR + Ref + Val + De;
+
+            SalLiq = SalB - DescFinal;
+
+            Console.WriteLine("O salário líquido do funcionário " + Nome + " é de R$" + SalLiq); 
+
+
+        }
+
+    }
+}
---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FolhaPag folhapag = new FolhaPag();
+
+            Console.WriteLine("Digite o nome do funcionário");
+            folhapag.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o salário");
+            folhapag.Sal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite as horas extras diurnas");
+            folhapag.Hed = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite as horas extras noturnas");
+            folhapag.Hen = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o número de dependentes");
+            folhapag.Nd = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o número de faltas em horas");
+            folhapag.Fal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o número de descontos eventuais");
+            folhapag.De = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o número de gastos com refeições feitas na empresa");
+            folhapag.Ref = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o número vales retirados durante o mês");
+            folhapag.Val = Convert.ToDouble(Console.ReadLine());
+
+            folhapag.Calc();
+        }
+    }
+}
