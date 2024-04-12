22e71ef2683d2e8228b27528f05fadfa2680c44f
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-04-12 12:44:28 -0300

    Add files via upload

---
+++ Exercicio3/Exercicio3/AlbumDeFotos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public class AlbumDeFotos
+    {
+        public int NTotalDePag { get; set; }
+        public int NTotalDeFt { get; set; }
+        public int NumDeFt { get; set; }
+
+       
+
+        public void Album() 
+        {
+            Console.WriteLine("Digite o número de páginas do seu álbum");
+            NTotalDePag = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o número de fotos que você deseja colocar");
+            NumDeFt = Convert.ToInt32(Console.ReadLine());
+
+            NTotalDeFt = NTotalDePag * 2;
+
+            if(NTotalDeFt < NumDeFt)
+            {
+                Console.WriteLine("Você não pode colocar " + NumDeFt + ", o máximo é " + NTotalDeFt + " fotos.");
+            }
+            else
+            {
+                Console.WriteLine("Você pode colocar suas fotos.");
+            }
+        }
+
+        
+
+    }
+}
---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlbumDeFotos albumDeFotos = new AlbumDeFotos();
+
+            
+
+            albumDeFotos.Album();
+        }
+    }
+}
